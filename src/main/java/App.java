777c637085015beb6ae1214ca2dc9ab78be557b7
Alissa Paluska and Abby Rolling <alissa.paluska@gmail.com>
2016-02-17 11:23:01 -0800

    refactor arrayList from backend

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,23 +12,17 @@ public class App {
 
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
-      model.put("allCds", request.session().attribute("allCds"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     post("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
-      ArrayList<Cd> allCds = request.session().attribute("allCds");
-      if (allCds == null) {
-        allCds = new ArrayList<Cd>();
-        request.session().attribute("allCds", allCds);
-      }
+
       String newTitle = request.queryParams("cdTitle");
       Cd newCd = new Cd(newTitle);
-      allCds.add(newCd);
 
-      model.put("allCds", allCds);
+      model.put("allCds", Cd.all());
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
